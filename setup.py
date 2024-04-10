3d3bb6252d0a9462df97abc8faa0f5bce3f66da3
ryangar888 <elonmute@gmail.com>
2024-04-10 10:31:10 +0700

    update

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='ilovetea',
+version='0.1.0',
+author='Elonmute',
+author_email='elonmute@gmail.com',
+description='This is first project on Tea protocol',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
